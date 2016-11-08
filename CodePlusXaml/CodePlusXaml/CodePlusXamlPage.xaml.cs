fc84281ed8b53b586604c5ae853e9898f0729a3f
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-08 16:22:42 +0530

    added the file

---
+++ CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CodePlusXaml
+{
+	public partial class CodePlusXamlPage : ContentPage
+	{
+		public CodePlusXamlPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
